6892910f43e5f6ec03aa92b340d01d607889af40
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-07-17 17:57:27 +0530

    feat : added up and down

--- direction.hpp
+++ direction.hpp
@@ -1,4 +1,4 @@
 #pragma once
-enum class Direction {Right , Left};
+enum class Direction { Up = 0, Left, Down, Right };
 // we need to make a separate header for this cause this is gonna be 
 // used in a lot of places
